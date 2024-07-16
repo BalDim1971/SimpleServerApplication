955ca048856c3a3e4031b3a58b0d7058648a6e3f
Дмитрий Балашов <korochun@yandex.ru>
2024-07-16 13:18:32 +0300

    Добавлена попытка сделать авторизацию по учебнику.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import uvicorn
 
-from sqlalchemy.orm import Session
-from typing import Union, Annotated
+# from sqlalchemy.orm import Session
+from typing import Annotated
 
-from fastapi import FastAPI, Depends
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+
 #
 # from auth.routes import api_auth
 # from src.config import DB_HOST, DB_PORT
@@ -17,9 +19,32 @@ from fastapi.security import OAuth2PasswordBearer
 #
 # Base.metadata.create_all(bind=engine)
 
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
+
+
 app = FastAPI(
     title="Простой сервер с авторизацией",
 )
+
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
@@ -32,14 +57,68 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 #     return {"message": "Welcome to FastAPI authentication and authorization example"}
 
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
 
 
-@app.get("/items/")
-async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
-    return {"token": token}
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
+    return user
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@app.get("/users/me")
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
+
 
 if __name__ == '__main__':
     # uvicorn.run(app, host=DB_HOST, port=DB_PORT)
