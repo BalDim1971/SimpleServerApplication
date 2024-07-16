39bb85fb12829559b65770b969cf3080a18b0228
Дмитрий Балашов <korochun@yandex.ru>
2024-07-16 15:58:26 +0300

    Продолжаем изучать FastAPI и авторизацию по учебнику.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import uvicorn
-
 # from sqlalchemy.orm import Session
 from typing import Annotated
 
-from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import FastAPI, Depends, HTTPException, status, Header
+from fastapi.security import (OAuth2PasswordBearer, OAuth2PasswordRequestForm,
+                              HTTPBasic, HTTPBasicCredentials)
 from pydantic import BaseModel
 
 #
@@ -19,34 +18,12 @@ from pydantic import BaseModel
 #
 # Base.metadata.create_all(bind=engine)
 
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
-        "disabled": False,
-    },
-    "alice": {
-        "username": "alice",
-        "full_name": "Alice Wonderson",
-        "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
-        "disabled": True,
-    },
-}
-
 
 app = FastAPI(
     title="Простой сервер с авторизацией",
 )
 
-
-def fake_hash_password(password: str):
-    return "fakehashed" + password
-
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
 # app.include_router(api_auth)
 # app.include_router(api_users)
@@ -57,69 +34,21 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 #     return {"message": "Welcome to FastAPI authentication and authorization example"}
 
 
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
+secret_user: str = "newphone"
+secret_password: str = "whodis?"
+
+basic: HTTPBasicCredentials = HTTPBasic()
 
 
-class UserInDB(User):
-    hashed_password: str
-
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-
-def fake_decode_token(token):
-    # This doesn't provide any security at all
-    # Check the next version
-    user = get_user(fake_users_db, token)
-    return user
-
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    return user
-
-
-async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)],
-):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
-
-
-@app.post("/token")
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-
-    return {"access_token": user.username, "token_type": "bearer"}
-
-
-@app.get("/users/me")
-async def read_users_me(
-    current_user: Annotated[User, Depends(get_current_active_user)],
-):
-    return current_user
+@app.get("/who")
+def get_user(creds: HTTPBasicCredentials = Depends(basic)) -> dict:
+    if (creds.username == secret_user and creds.password == secret_password):
+        return {"username": creds.username, "password": creds.password}
+    raise HTTPException(status_code=401, detail='Hey!')
 
 
 if __name__ == '__main__':
-    # uvicorn.run(app, host=DB_HOST, port=DB_PORT)
-    uvicorn.run(app)
+    import uvicorn
+
+    # uvicorn.run(app, host=DB_HOST, port=DB_PORT, reload=True)
+    uvicorn.run("main:app", reload=True)
